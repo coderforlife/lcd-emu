74111d71e38a81eb788f20d6b388976d2b4d3907
Jeffrey Bush <jeff@coderforlife.com>
2012-05-13 13:38:35 -0700

    Adding CGROMs for many common LCDs.

--- cgrom-conv/Program.cs
+++ cgrom-conv/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace cgrom_conv
@@ -12,8 +9,9 @@ namespace cgrom_conv
     {
         static void Main(string[] args)
         {
+            string filename = args.Length > 0 ? args[0] : "cgrom.txt";
+            StreamReader r = new StreamReader(File.OpenRead(filename));
             byte[] data = new byte[10 * 16 * 16];
-            StreamReader r = new StreamReader(File.OpenRead("cgrom.txt"));
             string line;
             int x = 0;
             while ((line = r.ReadLine()) != null)
@@ -34,7 +32,7 @@ namespace cgrom_conv
             for (int i = 0; i < data.Length; ++i)
                 Marshal.WriteByte(d.Scan0, i*d.Stride, data[i]);
             b.UnlockBits(d);
-            b.Save("cgrom.png");
+            b.Save(Path.ChangeExtension(filename, ".png"));
         }
     }
 }
